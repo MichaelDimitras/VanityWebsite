40a69b5ccb6c003e23604f16068531f3246a41df
Michael Dimitras <michaeladimitras@gmail.com>
2024-02-01 17:35:35 -0800

    page style changes

--- vanity-website/src/app/page.tsx
+++ vanity-website/src/app/page.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
+import "./page.css";
 
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <div className="">
+      <div className="profile-container">
         <Image
-          className="rounded-full"
+          className="rounded-full shadow-lg mx-auto"
           src="/profile.jpg"
           alt="Michael Dimitras profile picture"
           width={400}
           height={300}
           priority
         />
+        <p className="profile-caption">Coming Soon</p>
       </div>
-      <h2 className={`mb-3 text-2xl font-semibold`}>Coming Soon</h2>
     </main>
   );
 }
